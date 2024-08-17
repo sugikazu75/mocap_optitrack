b3ec438472e256d4e2bf797a865901cd2def54dc
LI, Jinjie <li-jinjie@g.ecc.u-tokyo.ac.jp>
2024-08-17 16:37:24 +0900

    add a destruction function; correct typos

--- src/mocap_node.cpp
+++ src/mocap_node.cpp
@@ -57,6 +57,10 @@ public:
     publisherConfigurations = pubConfigs;
   }
 
+  ~OptiTrackRosBridge() {
+    multicastClientSocketPtr.reset();
+  };
+
   void reconfigureCallback(MocapOptitrackConfig& config, uint32_t)
   {
     serverDescription.enableOptitrack = config.enable_optitrack;
@@ -81,9 +85,9 @@ public:
         dataModel.setVersions(&serverDescription.version[0], &serverDescription.version[0]);
       }
 
-      // Need verion information from the server to properly decode any of their packets.
-      // If we have not recieved that yet, send another request.
-      while (ros::ok() && !dataModel.hasServerInfo())
+      // Need version information from the server to properly decode any of their packets.
+      // If we have not received that yet, send another request.
+      while (ros::ok())
       {
         natnet::ConnectionRequestMessage connectionRequestMsg;
         natnet::MessageBuffer connectionRequestMsgBuffer;
@@ -96,6 +100,9 @@ public:
         else sleep(1);
 
         ros::spinOnce();
+
+        if (dataModel.hasServerInfo())
+          break;
       }
       // Once we have the server info, create publishers
       publishDispatcherPtr.reset(
